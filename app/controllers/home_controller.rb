d7f0916f13b6209c82c31b718ead2f12fc223aa8
Jean Felipe Sorio <jeanfsorio@gmail.com>
2022-12-04 08:46:25 -0300

    local mongo host and json as backup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
     def index
         @resultados = Resultado.all.select{ |item| item[:pst].to_f > 0.0 }.sort_by!{|r| r.datetime.to_i}.reverse
         @localidades = Localidade.all
-        @resultado = Resultado.find("635f4277213c0deaa770bedf") #ultimo de presidente
+        #@resultado = Resultado.find("635f4277213c0deaa770bedf") #ultimo de presidente
     end
     def resultado
         @resultado = Resultado.find(params[:id])
